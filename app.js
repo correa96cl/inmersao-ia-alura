6c5680ea9c289d18a7c445fbe1b9b060deae660c
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-09-06 14:03:39 -0300

    Commit final

--- app.js
+++ app.js
@@ -17,31 +17,39 @@ link.href = 'style-claro.css';
 document.head.appendChild(link);
 
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-    const inputDoenca = document.getElementById("doenca");
-    // Selecionar todos os elementos <p>
-  
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  const inputDoenca = document.getElementById("doenca");
+  // Selecionar todos os elementos <p>
+
+
+
+
+
+  if (inputDoenca.value != "") {
+    const paragraphs = document.querySelectorAll('p');
+
+    paragraphs.forEach((p) => {
+      if (p.ariaLabel == "validar") {
+        p.remove();
+      }
+    });
+    inputDoenca.removeAttribute("style");
+
     
-
-
-
-    if (inputDoenca.value != "") {
-        const paragraphs = document.querySelectorAll('p');
-
-        paragraphs.forEach((p) => {
-            if (p.ariaLabel == "validar") {
-                p.remove();
-            }
-        });
-        inputDoenca.removeAttribute("style");
-        // Itera sobre cada dado da lista de dados
-        for (let dado of dados) {
-            // Cria um novo elemento HTML para cada resultado
-            resultados += `
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+      // Cria um novo elemento HTML para cada resultado
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      console.log(dado.tags)
+      tags = dado.tags.toLowerCase()
+      if (titulo.includes(inputDoenca.value.toLowerCase()) 
+        || descricao.includes(inputDoenca.value.toLowerCase()) || tags.includes(inputDoenca.value.toLowerCase())) {
+        resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -50,51 +58,58 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
-        }
-    } else {
-        const paragraphs = document.querySelectorAll('p');
-
-        paragraphs.forEach((p) => {
-            if (p.ariaLabel == "validar") {
-                p.remove();
-            }
-        });
-        const novoParagrafo = document.createElement("p");
-        
-        inputDoenca.style.border = "1px solid red";
-        novoParagrafo.ariaLabel = "validar";
-        novoParagrafo.style.color = "indianred";
-        novoParagrafo.style.margin = "0 0 10px 0";
-        novoParagrafo.textContent = "Deve preencher com um nome de doenca.";
-        inputDoenca.parentNode.insertBefore(novoParagrafo, inputDoenca.nextSibling);
-
-
+      }
+     
     }
-
-    // Atribui os resultados gerados à seção HTML
+    if (!resultados){
+      resultados += `
+      <p style="color: #45474B; margin: 0 0 10px 0">Nenhum resultado encontrado</p>`
+    }
     section.innerHTML = resultados;
+  } else {
+    const paragraphs = document.querySelectorAll('p');
+
+    paragraphs.forEach((p) => {
+      if (p.ariaLabel == "validar") {
+        p.remove();
+      }
+    });
+    const novoParagrafo = document.createElement("p");
+
+    inputDoenca.style.border = "1px solid red";
+    novoParagrafo.ariaLabel = "validar";
+    novoParagrafo.style.color = "indianred";
+    novoParagrafo.style.margin = "0 0 10px 0";
+    novoParagrafo.textContent = "Deve preencher com um nome de doenca.";
+    inputDoenca.parentNode.insertBefore(novoParagrafo, inputDoenca.nextSibling);
+
+
+  }
+
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
 }
 
 
 function cambiarTema(tema) {
-    const link = document.getElementById('estilo');
-    link.href = tema === 'claro' ? 'style-claro.css' : 'style-oscuro.css';
-  }
+  const link = document.getElementById('estilo');
+  link.href = tema === 'claro' ? 'style-claro.css' : 'style-oscuro.css';
+}
 
-  // Detectar la hora del día y cambiar el tema automáticamente (opcional)
-  const hora = new Date().getHours();
-  console.log(hora);
-  if (hora >= 14 || hora < 6) {
-    cambiarTema('oscuro');
-  }
+// Detectar la hora del día y cambiar el tema automáticamente (opcional)
+const hora = new Date().getHours();
+console.log(hora);
+if (hora >= 14 || hora < 6) {
+  cambiarTema('oscuro');
+}
 
-  function toggleCSS() {
-    const estilo = document.getElementById('estilo');
-    const checkbox = document.getElementById('toggleEstilo');
-  
-    if (checkbox.checked) {
-      estilo.href = "style-claro.css";
-    } else {
-      estilo.href = "style-oscuro.css";
-    }
+function toggleCSS() {
+  const estilo = document.getElementById('estilo');
+  const checkbox = document.getElementById('toggleEstilo');
+
+  if (checkbox.checked) {
+    estilo.href = "style-claro.css";
+  } else {
+    estilo.href = "style-oscuro.css";
   }
+}
--- dados.js
+++ dados.js
@@ -2,21 +2,25 @@ let dados = [
     {
         titulo: "Cancer",
         descricao: "O câncer é um termo abrangente que engloba um grande grupo de doenças caracterizadas pelo crescimento anormal e descontrolado das células. Essas células podem invadir tecidos adjacentes e se espalhar para outras partes do corpo.",
-        link: "https://www.gov.br/inca"
+        link: "https://www.gov.br/inca",
+        tags: "cancer quimio radioterapia"
     },
     {
         titulo: "HIV",
         descricao: "O HIV (Vírus da Imunodeficiência Humana) é um retrovírus que ataca o sistema imunológico, comprometendo a capacidade do corpo de se defender contra infecções. Essa doença se manifesta em estágios e pode levar à AIDS (Síndrome da Imunodeficiência Adquirida), uma condição mais avançada em que o sistema imunológico está severamente debilitado.",
-        link: "https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/a/aids-hiv"
+        link: "https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/a/aids-hiv",
+        tags: "hiv aids sexo"
     },
     {
         titulo: "MPOX",
         descricao: "O Mpox, também conhecido como varíola dos macacos, é uma doença infecciosa causada pelo vírus mpox. Embora menos conhecida do público em geral até recentemente, essa doença existe há décadas, principalmente em regiões da África",
-        link: "https://www.who.int/es/news-room/fact-sheets/detail/mpox"
+        link: "https://www.who.int/es/news-room/fact-sheets/detail/mpox",
+        tags: "variola macaco africa"
     },
     {
         titulo: "COVID-19",
         descricao: "A COVID-19, causada pelo vírus SARS-CoV-2, é uma doença infecciosa que se espalhou rapidamente pelo mundo, causando uma pandemia.",
-        link: "https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+        link: "https://www.who.int/emergencies/diseases/novel-coronavirus-2019",
+        tags: "china pfizer pandemia covid"
     }
 ];
